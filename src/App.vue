f4686c4b1a59f27e485a08fa02cd4c619dd86ac8
sieunp06 <psieun0606@gmail.com>
2025-05-10 12:10:55 +0900

    feat: 푸터 구현

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
     <AppHeader />
     <router-view />
+    <AppFooter />
 </template>
 
 <script>
 import AppHeader from "@/components/fragments/AppHeader.vue";
+import AppFooter from "@/components/fragments/AppFooter.vue";
 
 export default {
   components: {
     AppHeader,
+    AppFooter,
   },
 };
 </script>
