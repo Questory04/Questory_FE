c0ef72b2e7424265eb251cd761b6ae6f135c499a
sieunp06 <psieun0606@gmail.com>
2025-05-12 13:42:25 +0900

    config: mdi/font 설정 추가

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+import { createVuetify } from "vuetify";
+import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+const vuetify = createVuetify({
+    components,
+    directives,
+});
+
+createApp(App).use(router).use(vuetify).mount("#app");
