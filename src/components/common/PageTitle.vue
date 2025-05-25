4547326cfa43677ce25295e9529e29b96cff2dbe
sieunp06 <psieun0606@gmail.com>
2025-05-26 01:57:22 +0900

    feat: 게시판 글 전체 조회 페이지 구현

---
+++ src/components/common/PageTitle.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="page-title-block">
+    <h2 class="title">{{ title }}</h2>
+    <div class="title-underline"></div>
+    <p v-if="subtitle" class="description">{{ subtitle }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    subtitle: String
+  }
+}
+</script>
+
+<style scoped>
+.page-title-block {
+  text-align: center;
+  margin-bottom: 2rem;
+}
+
+.title {
+  font-size: 1.8rem;
+  color: #5a9bd8;
+  margin-top: 1rem;
+}
+
+.title-underline {
+  width: 200px;
+  height: 3px;
+  background-color: #5a9bd8;
+  margin: 0.5rem auto 1.5rem;
+}
+
+.description {
+  color: #666;
+  font-size: 0.95rem;
+}
+</style>
