824c32feafbeefd7b00478f9d6c3be5e1a5704ab
ahyoon99 <kimay9623@naver.com>
2025-05-26 07:48:18 +0900

    feat: 장소 검색하기

--- src/router/planRoutes.js
+++ src/router/planRoutes.js
@@ -1,14 +1,28 @@
 export default [
     {
-        path: '/plans/create',
-        name: 'PlanCreateForm',
+        path: "/plans/create",
+        name: "PlanCreateForm",
         component: () => import("@/views/plan/PlanCreatePage.vue"),
     },
     {
         // path: '/plans/:planId/edit',
-        path: '/plans/edit',
-        name: 'PlanRouteEditor',
+        path: "/plans/edit",
+        name: "PlanRouteEditor",
         component: () => import("@/views/plan/PlanRouteEditor.vue"),
+        props: true,
+    },
+    {
+        // path: '/plans/:planId/edit',
+        path: "/plans/edit2",
+        name: "PlanRouteEditor2",
+        component: () => import("@/views/plan/PlanRouteEditorTest.vue"),
+        props: true,
+    },
+    {
+        // path: '/plans/:planId/edit',
+        path: "/plans/:planId/edit4",
+        name: "PlanRouteEditor3",
+        component: () => import("@/views/plan/PlanRouteEditorTest.vue"),
         props: true,
     },
 ];
