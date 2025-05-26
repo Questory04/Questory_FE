9ca97f84e5f712d531f5c1687d80ea1fda2bfd6f
ahyoon99 <kimay9623@naver.com>
2025-05-27 04:07:52 +0900

    fix: view 파일명 맨 앞 대문자로 수정

--- src/router/planRoutes.js
+++ src/router/planRoutes.js
@@ -14,13 +14,13 @@ export default [
     {
         path: "/plans",
         name: "getPlansList",
-        component: () => import("@/views/plan/getPlansListPage.vue"),
+        component: () => import("@/views/plan/GetPlansListPage.vue"),
         props: true,
     },
     {
         path: "/plans/:planId",
         name: "getPlanDetail",
-        component: () => import("@/views/plan/getPlanDetailPage.vue"),
+        component: () => import("@/views/plan/GetPlanDetailPage.vue"),
         props: true,
     },
     {
